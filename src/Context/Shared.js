3ed83beb075b9d22d5c65e8b89f8213a12c62f17
Roberto Work <rusvelt.rr@gmail.com>
2024-04-10 16:00:08 +1000

    fixed logo dark theme

--- src/Context/Shared.js
+++ src/Context/Shared.js
@@ -4,6 +4,7 @@ import logotype from "../Assets/Logotype.svg";
 const SharedContext = createContext();
 
 function SharedProvider({ children }) {
+  //The following updates the logo based on the theme
   const [logoImage, setLogoImage] = useState(logotype);
   const updateLogoImage = (logo) => {
     setLogoImage(logo);
@@ -15,7 +16,7 @@ function SharedProvider({ children }) {
   };
 
   return (
-    <SharedContext.Provider value={sharedContextValue}>
+    <SharedContext.Provider value={{ logoImage, updateLogoImage }}>
       {children}
     </SharedContext.Provider>
   );
