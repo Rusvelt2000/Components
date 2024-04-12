d842ea1a152cdbcf4f6e4b5252230e6435c00343
Roberto Work <rusvelt.rr@gmail.com>
2024-04-12 16:09:50 +1000

    implementing sort

--- src/Components/SortableTable.js
+++ src/Components/SortableTable.js
@@ -1,9 +1,26 @@
+import { useState } from "react";
 import Table from "./Table";
 
 function SortableTable(props) {
+  const [sortBy, setSortBy] = useState(null);
+  const [sortOrder, setSortOrder] = useState(null);
   const { config } = props;
   const handleClick = (label) => {
-    console.log(label);
+    console.log(`if ${sortBy} !== ${label}`);
+    if (sortBy !== label) {
+      setSortOrder("asc");
+    }
+
+    setSortBy(label);
+    console.log(sortOrder);
+
+    if (sortOrder === "asc") {
+      setSortOrder("desc");
+    } else if (sortOrder === "desc") {
+      setSortOrder(null);
+    } else if (sortOrder === null) {
+      setSortOrder("asc");
+    }
   };
   const sortableConfig = config.map((column, index) => {
     if (column.isSortable) {
@@ -24,7 +41,12 @@ function SortableTable(props) {
     }
   });
 
-  return <Table {...props} config={sortableConfig} />;
+  return (
+    <div>
+      {sortOrder} - {sortBy}
+      <Table {...props} config={sortableConfig} />;
+    </div>
+  );
 }
 
 export default SortableTable;
