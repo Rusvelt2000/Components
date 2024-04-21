ee87fe77d79f74a6cb311c529d779b458f1d9ca4
Rob <rusvelt.rr@gmail.com>
2024-04-21 19:24:12 +1000

    finished placeholders

--- src/Components/Placeholder.js
+++ src/Components/Placeholder.js
@@ -1,19 +1,43 @@
 import classNames from "classnames";
 
-function Placeholder({ xs, sm, md, lg, xl, image, button, fix }) {
+function Placeholder({
+  xs,
+  sm,
+  md,
+  lg,
+  xl,
+  emptyLine,
+  image,
+  avatar,
+  chat,
+  left,
+  right,
+  button,
+  fix,
+}) {
   const classes = classNames("Placeholder", "glow", {
     xs,
     sm,
     md,
     lg,
     xl,
+    emptyLine,
     image,
+    avatar,
+    chat,
+    left,
+    right,
     button,
     fix,
   });
+  const chatWidth = Math.floor(Math.random() * (280 - 80) + 60);
   return (
     <div className="PlaceholderContainer">
-      <div className={classes}></div>
+      {chat ? (
+        <div style={{ width: chatWidth }} className={classes}></div>
+      ) : (
+        <div className={classes}></div>
+      )}
     </div>
   );
 }
--- src/Pages/PlaceholderPage.js
+++ src/Pages/PlaceholderPage.js
@@ -6,12 +6,34 @@ function PlaceholderPage() {
   return (
     <div>
       <Title>Placeholders</Title>
-      <div className="fade-in-up col-2">
-        <Container title={"Default placeholder"}>
-          <Placeholder md image />
+      <div className="fade-in-up col-3">
+        <Container title={"Texts"}>
           <Placeholder />
           <Placeholder xl />
+          <Placeholder md />
+          <Placeholder emptyLine />
+          <Placeholder lg />
+          <Placeholder xs />
+          <Placeholder emptyLine />
+          <Placeholder />
+          <Placeholder md />
+        </Container>
+        <Container title={"Chat Bubbles"}>
+          <Placeholder chat left />
+          <Placeholder chat left />
+          <Placeholder chat right />
+          <Placeholder chat right />
+          <Placeholder chat left />
+        </Container>
+
+        <Container title={"Avatar and Buttons"}>
+          <Placeholder avatar />
           <Placeholder button sm />
+          <Placeholder button md />
+          <Placeholder button lg />
+        </Container>
+        <Container title={"Images"}>
+          <Placeholder image />
         </Container>
       </div>
     </div>
