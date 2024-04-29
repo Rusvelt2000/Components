0008aa0e631d2661d616dcd42cc5a77686a1770e
Roberto Work <rusvelt.rr@gmail.com>
2024-04-29 16:59:47 +1000

    connected store to app

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./store";
+import { store } from "./store";
+import { Provider } from "react-redux";
 import { NavigationProvider } from "./Context/Navigation";
 import { SharedProvider } from "./Context/Shared";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <SharedProvider>
-    <NavigationProvider>
-      <App />
-    </NavigationProvider>
-  </SharedProvider>
+  <Provider store={store}>
+    <SharedProvider>
+      <NavigationProvider>
+        <App />
+      </NavigationProvider>
+    </SharedProvider>
+  </Provider>
 );
